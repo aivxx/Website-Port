f9764753a1893e4196d37bff38e85858c45a6fef
aivxx <90653195+aivxx@users.noreply.github.com>
2021-11-13 19:08:04 -0500

    css edit
    scrapped other nav bar, imported responsive nav bar, edit all other content to be responsive

--- js/script.js
+++ js/script.js
@@ -1,38 +1,49 @@
-var $els = $(".menu a, .menu header");
-var count = $els.length;
-var grouplength = Math.ceil(count / 3);
-var groupNumber = 0;
-var i = 1;
-$(".menu").css("--count", count + "");
-$els.each(function (j) {
-  if (i > grouplength) {
-    groupNumber++;
-    i = 1;
+let facebookBTN = document.querySelector(".fa-facebook");
+let gitBTN = document.querySelector(".fa-github");
+let instaBTN = document.querySelector(".fa-instagram");
+let linkedBTN = document.querySelector(".fa-linkedin");
+
+facebookBTN.addEventListener(`click`, () => {
+  window.open("https://www.facebook.com/ashleydiego");
+});
+
+gitBTN.addEventListener(`click`, () => {
+  window.open("https://github.com/aivxx");
+});
+
+instaBTN.addEventListener(`click`, () => {
+  window.open("https://www.instagram.com/ashleyrebeccaa_/");
+});
+
+linkedBTN.addEventListener(`click`, () => {
+  window.open("https://www.linkedin.com/in/ashley-goldstein-40bb15183/");
+});
+
+// define all UI variable
+const navToggler = document.querySelector(".nav-toggler");
+const navMenu = document.querySelector(".site-navbar ul");
+const navLinks = document.querySelectorAll(".site-navbar a");
+
+// load all event listners
+allEventListners();
+
+// functions of all event listners
+function allEventListners() {
+  // toggler icon click event
+  navToggler.addEventListener("click", togglerClick);
+  // nav links click event
+  navLinks.forEach((elem) => elem.addEventListener("click", navLinkClick));
+}
+
+// togglerClick function
+function togglerClick() {
+  navToggler.classList.toggle("toggler-open");
+  navMenu.classList.toggle("open");
+}
+
+// navLinkClick function
+function navLinkClick() {
+  if (navMenu.classList.contains("open")) {
+    navToggler.click();
   }
-  $(this).attr("data-group", groupNumber);
-  i++;
-});
-
-$(".menu footer button").on("click", function (e) {
-  e.preventDefault();
-  $els.each(function (j) {
-    $(this).css(
-      "--top",
-      $(this)[0].getBoundingClientRect().top +
-        $(this).attr("data-group") * -15 -
-        20
-    );
-    $(this).css("--delay-in", j * 0.1 + "s");
-    $(this).css("--delay-out", (count - j) * 0.1 + "s");
-  });
-  $(".menu").toggleClass("closed");
-  e.stopPropagation();
-});
-
-// run animation once at beginning for demo
-setTimeout(function () {
-  $(".menu footer button").click();
-  setTimeout(function () {
-    $(".menu footer button").click();
-  }, count * 100 + 500);
-}, 1000);
+}
